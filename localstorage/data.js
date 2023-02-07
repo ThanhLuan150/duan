869190d1745095844f46e09797c00a1313a7c1d2
ThanhLuan150 <letruongthanhluan1505@gmail.com>
2023-02-07 15:37:46 +0700

    sdsdsd

---
+++ localstorage/data.js
@@ -0,0 +1,68 @@
+const  man=[
+    {
+        id:1,
+        name:"Áo thun nam phông rộng",
+        code:"TC1025011BA",
+        price:"250.000",
+        image: "https://cf.shopee.vn/file/c1bc85e969540e23e2b35b55a887765e"
+    },
+    {
+        id:2,
+        name:"Áo thun nam phông rộng ",
+        code:"TC1025111BA",
+        price:"250.000",
+        image: "https://cf.shopee.vn/file/c1bc85e969540e23e2b35b55a887765e"
+    },
+    {
+        id:1,
+        name:"Áo thun nam phông rộng",
+        code:"TC1025011BA",
+        price:"250.000",
+        image: "https://cf.shopee.vn/file/c1bc85e969540e23e2b35b55a887765e"
+    },
+    {
+        id:1,
+        name:"Áo thun nam phông rộng",
+        code:"TC1025011BA",
+        price:"250.000",
+        image: "https://cf.shopee.vn/file/c1bc85e969540e23e2b35b55a887765e"
+    }
+    
+]
+
+localStorage.setItem("listProductMan",JSON.stringify(man));
+
+
+
+var women=[
+    {
+        id:1,
+        name:"Quần jean ống rộng nữ ",
+        code:"TC1025011BA",
+        price:"250.000",
+        image: "https://luvinus.com/wp-content/uploads/2019/09/cach-phoi-do-voi-quan-jean-ong-rong-24-820x1024.jpg"
+    },
+    {
+        id:2,
+        name:"Quần jean ống rộng nữ",
+        code:"TC1025111BA",
+        price:"250.000",
+        image: "https://luvinus.com/wp-content/uploads/2019/09/cach-phoi-do-voi-quan-jean-ong-rong-24-820x1024.jpg"
+    },
+    {
+        id:1,
+        name:"Quần jean ống rộng nữ",
+        code:"TC1025011BA",
+        price:"250.000",
+        image: "https://luvinus.com/wp-content/uploads/2019/09/cach-phoi-do-voi-quan-jean-ong-rong-24-820x1024.jpg"
+    },
+    {
+        id:1,
+        name:"Quần jean ống rộng nữ",
+        code:"TC1025011BA",
+        price:"250.000",
+        image: "https://luvinus.com/wp-content/uploads/2019/09/cach-phoi-do-voi-quan-jean-ong-rong-24-820x1024.jpg"
+    }
+];
+
+localStorage.setItem("listProductWoman",JSON.stringify(women));
